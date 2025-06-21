6806b09d93ee7d97a814c335be4c5ec0b06abb58
aleksandr105 <aleksandrsherbina105@gmail.com>
2025-06-21 22:52:46 +0200

    added table for murkap

--- index.js
+++ index.js
@@ -25,21 +25,35 @@ const getTime = () => {
 
 const createMurkup = async () => {
   const { data } = await getFlyData(...getTime());
-
+  console.log(data);
   if (!data) return;
 
   const murkup = data
     .map(({ airline_name, airline_logo, airport, status }) => {
       if (status === '') return '';
+
+      let backgroundStatus = 'tr-flight';
+
+      switch (status.includes('WYLĄDOWAŁ') ? 'WYLĄDOWAŁ' : status) {
+        case 'WYLĄDOWAŁ':
+          backgroundStatus = 'plane-landed';
+          break;
+        case 'OPÓŹNIONY':
+          backgroundStatus = 'delayed-landed';
+          break;
+        default:
+          backgroundStatus = 'tr-flight';
+      }
+
       return `
-    <li class="flight-item">
-      <p class="flight-status">${status}</p>
-      <img class="flight-logo" src="${airline_logo}" alt="${airline_name} logo">
-      <p class="flight-airport">${airport}</p>
-    </li>`;
+    <tr class=${backgroundStatus}>
+      <td class="td-flight-status">${status}</td>
+      <td><img class="td-flight-logo" src="${airline_logo}" alt="${airline_name} logo"></td>
+      <td class="td-flight-airport">${airport}</td>
+    </tr>`;
     })
     .join('');
-  const listRef = document.querySelector('.flight-list');
+  const listRef = document.querySelector('.table-tbody');
   listRef.innerHTML = murkup;
 };
 createMurkup();
