1f942b7c7903ab282dd62f1b1848c8fd35c4e005
Aleksandr <aleksandrsherbina105@gmail.com>
2025-06-24 03:12:26 +0200

    added translate lang

--- index.js
+++ index.js
@@ -1,31 +1,8 @@
-const getFlyData = async (date, time_from, time_to) => {
-  return await fetch(
-    `https://www.katowice-airport.com/pl/api/flight-board/list?direction=2&date=${date}&time_from=${time_from}&time_to=${time_to}`
-  )
-    .then(data => data.json())
-    .then(data => data)
-    .catch(err => {
-      console.error('Ошибка при загрузке данных:', err);
-    });
-};
-
-const getTime = () => {
-  const date = new Date();
-  const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const day = date.getDate().toString().padStart(2, '0');
-
-  const hours = date.getHours() === 0 ? '23' : (date.getHours() - 1).toString().padStart(2, '0');
-  const minutes = date.getMinutes().toString().padStart(2, '0');
-  const dateFiltered = `${year}-${month}-${day}`;
-  const time_from = `${hours}:${minutes}`;
-  const timeTo = `23:59`;
-  return [dateFiltered, time_from, timeTo];
-};
+import { getFlyData, getTime } from './healpers/index.js';
 
 const createMurkup = async () => {
   const { data } = await getFlyData(...getTime());
-  console.log(data);
+
   if (!data) return;
 
   const murkup = data
---
+++ translation.js
@@ -0,0 +1,36 @@
+// Object.defineProperty(navigator, 'language', { get: () => 'uk-UA' });
+
+const userLang = navigator.language.split('-')[0];
+
+export const translations = {
+  en: {
+    status: 'Status',
+    airline_name: 'Airline',
+    airport: 'From',
+  },
+  pl: {
+    status: 'Status',
+    airline_name: 'Linia lotnicza',
+    airport: 'Skąd',
+  },
+  uk: {
+    status: 'Статус',
+    airline_name: 'Авіакомпанія',
+    airport: 'Звідки',
+  },
+  ru: {
+    status: 'Статус',
+    airline_name: 'Авиакомпания',
+    airport: 'Откуда',
+  },
+};
+
+const getTranslate = el => {
+  return (
+    (translations && translations[userLang] && translations[userLang][el]) || translations.en[el]
+  );
+};
+
+document.querySelector('.table-head-status').textContent = getTranslate('status');
+document.querySelector('.th-awialine').textContent = getTranslate('airline_name');
+document.querySelector('.table-head-airport').textContent = getTranslate('airport');
